37b175f0eaaf3cb0aa08aea2efe5c5fbf15f6876
pic <piccinini@gmail.com>
2011-04-05 16:21:30 +0200

    added VODs block for user page

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ config.to_prepare do
 end
 
 Redmine::Plugin.register :redmine_journal_center do
-  name 'Redmin Journal Center'
+  name 'Redmine Journal Center'
   description 'This plugin adds a view where user can check journal updates like in an inbox'
   version '0.0.1'
   url 'http://github.com/pic/redmine_journal_center'
